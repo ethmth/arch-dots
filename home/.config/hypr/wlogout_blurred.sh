48519efcedc97dd631c6217d50691bf028478fa1
ethmth <ethan@smaze.net>
2024-08-24 17:48:42 -0400

    Updated wlogout script

--- home/.config/hypr/wlogout_blurred.sh
+++ home/.config/hypr/wlogout_blurred.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-grim /tmp/shot.png
+
+MONITOR=$(hyprctl activeworkspace | grep monitor | grep -v "monitorID:" | rev | cut -d' ' -f1 | rev | cut -d':' -f1 | tr -s ' ')
+grim -o $MONITOR /tmp/shot.png
 wlogout --protocol layer-shell -b 5 -T 400 -B 400
 #magick /tmp/shot.png -blur 0x8 /tmp/shot_blurred.png
+
